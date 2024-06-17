95fea513f366048c24a2ab542d492de4db3302b4
mtz606 <mtzsunder606@gmail.com>
2024-06-17 13:41:33 -0400

    finished authorization

--- go.mod
+++ go.mod
@@ -1,9 +1,15 @@
-module github.com/mtz-606/rssagg
+module github.com/mtz606/rssagg
 
 go 1.22.4
 
 require (
 	github.com/go-chi/chi v1.5.5
-	github.com/joho/godotenv v1.5.1
 	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+
+)
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-	"github.com/mattzhong/rssagg/internal/database"
+	"github.com/mtz606/rssagg/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
@@ -25,8 +28,21 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	router := chi.NewRouter()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
 
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
+	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -39,7 +55,10 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
-
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
@@ -49,7 +68,7 @@ func main() {
 
 	log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
